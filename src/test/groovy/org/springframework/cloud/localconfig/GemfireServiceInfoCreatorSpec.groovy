40a5ab62d5dde9550395572d530cbad7bd689f30
Eitan Suez <eitan.suez@gmail.com>
2016-01-15 13:32:22 -0600

    added another unit test

--- src/test/groovy/org/springframework/cloud/localconfig/GemfireServiceInfoCreatorSpec.groovy
+++ src/test/groovy/org/springframework/cloud/localconfig/GemfireServiceInfoCreatorSpec.groovy
@@ -1,17 +1,29 @@
 package org.springframework.cloud.localconfig
 
+import org.springframework.cloud.service.UriBasedServiceData
 import spock.lang.Specification
 
 /**
  * Created by esuez on 1/15/16.
  */
 class GemfireServiceInfoCreatorSpec extends Specification {
+
+  def "should register gemfire as scheme"() {
+    when:
+    def serviceCreator = new GemfireServiceInfoCreator()
+
+    then:
+    serviceCreator.accept(new UriBasedServiceData("some-service", "gemfire://localhost:10334/")) == true
+  }
+
   def "should produce locator string from uri"() {
     expect:
-    GemfireServiceInfoCreator.extractLocatorFromUri("gemfire://localhost:1234") == "localhost[1234]"
+    GemfireServiceInfoCreator.extractLocatorFromUri("gemfire://localhost:10334") == "localhost[10334]"
   }
+
   def "should produce locator string from uri with trailing slash"() {
     expect:
-    GemfireServiceInfoCreator.extractLocatorFromUri("gemfire://localhost:1234/") == "localhost[1234]"
+    GemfireServiceInfoCreator.extractLocatorFromUri("gemfire://localhost:10334/") == "localhost[10334]"
   }
+
 }
